8e85821f6dfa095f19c2c2d344e6340d0d4da016
Etienne <etienne@weglot.com>
2018-11-13 10:46:29 +0100

    Fixed a bug in configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -27,8 +27,8 @@ class Configuration implements ConfigurationInterface
             ->children()
             ->scalarNode('api_key')->isRequired()->end()
             ->scalarNode('api_secret')->isRequired()->end()
-            ->enumMode('textmaster_env')
-            ->values('staging', 'production')
+            ->enumNode('textmaster_env')
+            ->values(['staging', 'production'])
             ->isRequired()
             ->end()
             ->end();
