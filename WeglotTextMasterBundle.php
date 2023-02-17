8860f8b64def5ab1db1baedff4ba9d1c9f7dd085
Vincent Langlet <vincentlanglet@hotmail.fr>
2023-02-17 17:51:28 +0100

    Solve Symfony deprecation

--- WeglotTextMasterBundle.php
+++ WeglotTextMasterBundle.php
@@ -2,12 +2,13 @@
 
 namespace Weglot\TextMasterBundle;
 
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Weglot\TextMasterBundle\DependencyInjection\WeglotTextMasterExtension;
 
 class WeglotTextMasterBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ExtensionInterface
     {
         return new WeglotTextMasterExtension();
     }
